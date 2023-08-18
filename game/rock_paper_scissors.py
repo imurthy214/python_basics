176b9ab009ed3e9600f49204e8def2638042f53b
imurthy214 <ishanmurthy214@gmail.com>
2023-08-17 18:37:07 -0700

    added game stats

--- game/rock_paper_scissors.py
+++ game/rock_paper_scissors.py
@@ -1,7 +1,8 @@
 
 import random
 def game():
-
+    computer_win=0
+    player_win=0
     while (True):
         print("__________")
         print("1-->rock ")
@@ -15,20 +16,27 @@ def game():
             print("draw ")
         elif c == 1 and random_number == 2:
             print("computer wins ")
+            computer_win=computer_win+1
         elif c == 1 and random_number == 3:
             print("player wins ")
+            player_win=player_win+1
         elif c == 2 and random_number == 1:
             print("player wins ")
+            player_win = player_win + 1
         elif c == 2 and random_number == 3:
             print("computer wins ")
+            computer_win = computer_win + 1
         elif c == 3 and random_number == 1:
             print("computer wins ")
+            computer_win = computer_win + 1
         elif c == 3 and random_number == 2:
             print("player wins ")
+            player_win = player_win + 1
         elif c == 0:
             return
         else:
             print("invalid choice")
+        print(f"game stats: Computer: {computer_win} Player: {player_win}")
 
     print("this statement will only be executed if break is selected")
 
